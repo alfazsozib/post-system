36fd9570439cc368bfac9dd3265cfc39c242fc8f
alfazsozib <alfazsozib@gmail.com>
2026-07-16 04:07:24 +0600

    api set to get products --v1

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,50 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
 
 export const getProducts = async () => {
-  const res = await axios.get("https://dummyjson.com/products");
-  return res.data;
+  try {
+    const res = await axios.get("https://dummyjson.com/products");
+    return res.data;
+    
+  } catch (err) {
+    console.log(`Error ${err}`)
+    
+  }
+  
 };
+
+
+export const searchProducts = async (query) =>{
+  try{
+      const searchRes = await axios.get("https://dummyjson.com/products/search",{params: {q: query}})
+      return searchRes.data;
+
+    } catch(err){
+      console.log(`Error ${err}`)
+    }
+  
+}
+
+
+export const getCategories = async () =>{
+  try{
+      const categoryRes = await axios.get("https://dummyjson.com/products/categories")
+
+      return categoryRes.data;
+      
+    } catch(err){
+      console.log(`Error ${err}`)
+    }
+}
+
+
+export const getProductsByCategory = async (category) =>{
+  try{
+      const productByCatRes = await axios.get(`https://dummyjson.com/products/category/${category}`)
+      return productByCatRes.data;
+
+    } catch(err){
+      console.log(`Error ${err}`)
+    }
+  
+}
