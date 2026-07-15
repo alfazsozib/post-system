4ae8c037a37cba98051d5d7cc376c2df53698857
alfazsozib <alfazsozib@gmail.com>
2026-07-15 23:12:59 +0600

    shadecn added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
       <h1 className="text-red-400">Hi</h1>
+      <Button />
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
+
 
 export default defineConfig({
   plugins: [
@@ -9,4 +11,9 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
+  },
 })
