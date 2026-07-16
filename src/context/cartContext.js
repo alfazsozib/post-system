3af72a0b915b25d8bdf5335e7e0251e775b1d497
alfazsozib <alfazsozib@gmail.com>
2026-07-16 11:51:14 +0600

    cart reducer added to manage the add, remove and update cart

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -16,4 +16,43 @@ const loadInitialCart = () =>{
     }
 };
 
+const cartReducer = (state, action) =>{
+    switch (action.type) {
+        case "ADD_ITEM": {
+            const existsProduct = state.find((item) => item.id === action.payload.id);
 
+            if (existsProduct) {
+                return state.map((item) =>
+                item.id === action.payload.id
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+                );
+            }
+
+            return [...state, { ...action.payload, quantity: 1 }];
+        
+        }
+
+        case "REMOVE_ITEM":
+            return state.filter((item) => item.id !== action.payload.id);
+    
+        case "UPDATE_QUANTITY": {
+            const { id, quantity } = action.payload;
+
+            // if quantity droped to 0 or less than 0 it will remove the item 
+            if (quantity <= 0) {
+                return state.filter((item) => item.id !== id);
+            }
+        
+            return state.map((item) =>
+                item.id === id ? { ...item, quantity } : item
+            );
+            }
+        
+            case "CLEAR_CART":
+            return [];
+        
+        default:
+            break
+    }
+}
